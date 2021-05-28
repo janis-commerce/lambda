8304e880baf8f4764cb0a23c9cb9a61543bad494
Joaquín Ormaechea <joaquinormaechea@gmail.com>
2021-05-28 14:49:34 -0300

    Added missing getters and tests for base lambdas

--- tests/lambda-bases/lambda-with-client-and-payload.js
+++ tests/lambda-bases/lambda-with-client-and-payload.js
@@ -18,12 +18,45 @@ describe('Lambda bases', () => {
 			assert.deepStrictEqual(lambda.session, fakeSession);
 		});
 
-		it('Should have validation setters mustHaveClient and mustHavePayload set as true by default', () => {
+		it('Should have working getter and setter for data', () => {
+
+			const invocationData = { foo: 'bar' };
 
 			const lambda = new LambdaWithClientAndPayload();
 
+			lambda.data = invocationData;
+
+			assert.deepStrictEqual(lambda.data, invocationData);
+		});
+
+		it('Should have validation getter mustHaveClient set as true by default', () => {
+			const lambda = new LambdaWithClientAndPayload();
 			assert.deepStrictEqual(lambda.mustHaveClient, true);
+		});
+
+		it('Should have validation getter mustHavePayload set as true by default', () => {
+			const lambda = new LambdaWithClientAndPayload();
 			assert.deepStrictEqual(lambda.mustHavePayload, true);
+		});
+
+		it('Should have validation getter mustHaveUser set as false by default', () => {
+			const lambda = new LambdaWithClientAndPayload();
+			assert.deepStrictEqual(lambda.mustHaveUser, false);
+		});
+
+		it('Should have validation getter struct set as null by default', () => {
+			const lambda = new LambdaWithClientAndPayload();
+			assert.deepStrictEqual(lambda.struct, null);
+		});
+
+		it('Should have validate method resolving by default', () => {
+			const lambda = new LambdaWithClientAndPayload();
+			assert.doesNotReject(() => lambda.validate());
+		});
+
+		it('Should have process method resolving by default', () => {
+			const lambda = new LambdaWithClientAndPayload();
+			assert.doesNotReject(() => lambda.process());
 		});
 
 	});
--- tests/lambda-bases/lambda.js
+++ tests/lambda-bases/lambda.js
@@ -18,6 +18,47 @@ describe('Lambda bases', () => {
 			assert.deepStrictEqual(lambda.session, fakeSession);
 		});
 
+		it('Should have working getter and setter for data', () => {
+
+			const invocationData = { foo: 'bar' };
+
+			const lambda = new Lambda();
+
+			lambda.data = invocationData;
+
+			assert.deepStrictEqual(lambda.data, invocationData);
+		});
+
+		it('Should have validation getter mustHaveClient set as false by default', () => {
+			const lambda = new Lambda();
+			assert.deepStrictEqual(lambda.mustHaveClient, false);
+		});
+
+		it('Should have validation getter mustHavePayload set as false by default', () => {
+			const lambda = new Lambda();
+			assert.deepStrictEqual(lambda.mustHavePayload, false);
+		});
+
+		it('Should have validation getter mustHaveUser set as false by default', () => {
+			const lambda = new Lambda();
+			assert.deepStrictEqual(lambda.mustHaveUser, false);
+		});
+
+		it('Should have validation getter struct set as null by default', () => {
+			const lambda = new Lambda();
+			assert.deepStrictEqual(lambda.struct, null);
+		});
+
+		it('Should have validate method resolving by default', () => {
+			const lambda = new Lambda();
+			assert.doesNotReject(() => lambda.validate());
+		});
+
+		it('Should have process method resolving by default', () => {
+			const lambda = new Lambda();
+			assert.doesNotReject(() => lambda.process());
+		});
+
 	});
 
 });
